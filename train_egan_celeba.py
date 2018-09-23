fa2de552d3b8ee4aadc47d0c9a10cd24bb991ebe
Geza Kovacs <geza0kovacs@gmail.com>
2018-09-22 22:42:57 -0700

    working? but we are not adding the losses anymore

--- train_egan_celeba.py
+++ train_egan_celeba.py
@@ -15,12 +15,20 @@ import torch.backends.cudnn as cudnn
 import random
 import argparse
 import numpy as np
+from os import path
+import json
 
 parser = argparse.ArgumentParser(description='train SNDCGAN model')
-parser.add_argument('--cuda', action='store_true', help='enables cuda')
+#parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--cuda', type=bool, default=True, help='is cuda enabled')
 parser.add_argument('--gpu_ids', default=range(4), help='gpu ids: e.g. 0,1,2, 0,2.')
-parser.add_argument('--gpunum', default=0, help='gpu num: e.g. 0')
+parser.add_argument('--gpunum', default=3, help='gpu num: e.g. 0')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
+parser.add_argument('--earlyterminate', type=bool, default=False, help='use early termination')
+parser.add_argument('--presetlearningrate', type=bool, default=False, help='use preset learning rate')
+#parser.add_argument('--numdiscriminators', type=int, default=10, help='number of discriminators in the gang')
+#parser.add_argument('--discriminators', type=str, default='0123456789', help='list of enabled discriminators')
+parser.add_argument('--discriminators', type=str, default='0123456789', help='list of enabled discriminators')
 parser.add_argument('--n_dis', type=int, default=5, help='discriminator critic iters')
 parser.add_argument('--nz', type=int, default=88, help='dimension of lantent noise')
 parser.add_argument('--batchsize', type=int, default=64, help='training batch size')
@@ -48,6 +56,54 @@ _netG = models_egan._netG
 _netC = models_egan._netC
 _netE = models_egan._netE
 _netD_list = models_egan._netD_list
+
+def generate_learning_rate():
+  base = random.randrange(3, 6)
+  return 10 ** (-base)
+
+discriminator_indexes_enabled = [int(x) for x in opt.discriminators]
+new_netD_list = []
+for idx in discriminator_indexes_enabled:
+  new_netD_list.append(_netD_list[idx])
+_netD_list = new_netD_list
+
+#nd = int(opt.numdiscriminators)
+nd = len(_netD_list)
+#nd = len(_netD_list)
+lr_G = generate_learning_rate()
+lr_E = generate_learning_rate()
+lr_list = [generate_learning_rate() for x in range(nd)]
+if opt.presetlearningrate:
+  # start this set of learning rates we have found to work well with the 10 discriminators in models_egan_celeba and BCELoss
+  lr_list = [0.001, 0.000002, 0.0002, 0.000001, 0.0002, 0.003, 0.0002, 0.00001, 0.0001, 0.00001]
+  lr_G = 0.0002
+  lr_E = 0.0002
+  # end
+  #lr_list = [0.00001] * 10
+  #lr_list = [0.000005] * 10
+  #lr_G = 0.00005
+  #lr_E = 0.00005
+
+
+hyperparameters = {
+  'lr_list': lr_list,
+  'lr_G': lr_G,
+  'lr_E': lr_E,
+  'nd': nd,
+  'discriminators': opt.discriminators,
+}
+
+_netD_list = _netD_list[:nd]
+
+from copy_info_to_logdir import copy_info_to_logdir, copy_hyperparameters_to_logdir
+copy_info_to_logdir(logdir)
+copy_hyperparameters_to_logdir(logdir, hyperparameters)
+
+opts_file = open(path.join(logdir, 'opts.txt'), 'wt')
+opts_file.write(str(opt))
+opts_file.close()
+
+loss_outfile = open(path.join(logdir, 'losses.jsonl'), 'wt')
 
 import pickle
 import os.path
@@ -163,9 +219,16 @@ def weight_filler(m):
 n_dis = opt.n_dis
 nz = opt.nz
 
+#losses_list = ['W', 'BCE', 'W', 'BCE', 'W', 'BCE', 'W', 'BCE', 'W', 'BCE'][:nd]
+#losses_list = ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'][:nd]
+losses_list = ['BCE', 'BCE', 'BCE', 'BCE', 'BCE', 'BCE', 'BCE', 'BCE', 'BCE', 'BCE'][:nd]
+include_sigmoid_list = [(x == 'BCE') for x in losses_list]
+print('include_sigmoid_list is')
+print(include_sigmoid_list)
+
 G = _netG(nz, 3, opt.batchsize, context_vector_length)
 C = _netC(3, opt.batchsize, context_vector_length)
-SND_list = [_netD_x(3, opt.batchsize) for _netD_x in _netD_list]
+SND_list = [_netD_x(3, opt.batchsize, include_sigmoid) for _netD_x,include_sigmoid in zip(_netD_list, include_sigmoid_list)]
 nd = len(SND_list)
 E = _netE(3, opt.batchsize, nd, context_vector_length)
 print(G)
@@ -190,9 +253,11 @@ fixed_noise = Variable(fixed_noise)
 criterion = nn.BCELoss() 
 def w_loss_func_G(D_fake):
   return -torch.mean(D_fake)
+  #return torch.exp(torch.mean(D_fake))
 
 def w_loss_func_D(D_real, D_fake):
   return -(torch.mean(D_real) - torch.mean(D_fake))
+  #return torch.exp(torch.mean(D_real) - torch.mean(D_fake))
 
 dtype = torch.FloatTensor
 
@@ -212,18 +277,43 @@ if opt.cuda:
     dtype = torch.cuda.FloatTensor
     uniform = uniform.cuda()
 
-optimizerG = optim.Adam(G.parameters(), lr=0.0002, betas=(0, 0.9))
+optimizerG = optim.Adam(G.parameters(), lr=lr_G, betas=(0, 0.9))
+#optimizerG = optim.SGD(G.parameters(), lr=0.0002)
+# TODO change back to nozero
+#optimizerG = optim.Adam(G.parameters(), lr=0.0, betas=(0, 0.9))
 optimizerSND_list = []
 # TODO: hyperparam tuning here
-lr_list = [0.001, 0.000002, 0.0002, 0.000001, 0.0002, 0.003, 0.0002, 0.00001, 0.0001, 0.00001][:nd]
+#lr_list = [0.001, 0.000002, 0.0002, 0.000001, 0.0002, 0.003, 0.0002, 0.00001, 0.0001, 0.00001][:nd]
 for [SNDx, lrx] in zip(SND_list, lr_list):
     optimizerSNDx = optim.Adam(SNDx.parameters(), lr=lrx, betas=(0, 0.9))
     optimizerSND_list.append(optimizerSNDx)
-optimizerE = optim.Adam(E.parameters(), lr=0.0002, betas=(0, 0.9))
+# TODO change back to nonzero
+optimizerE = optim.Adam(E.parameters(), lr=lr_E, betas=(0, 0.9))
 
 kl_div_fcn = nn.KLDivLoss().cuda()
 l2_fcn = nn.MSELoss().cuda()
 
+
+loss_D_history = []
+loss_G_history = []
+num_increases_tolerated = 20
+
+def append_and_ensure_length(arr, item):
+  arr.append(item)
+  while len(arr) > num_increases_tolerated:
+    arr.pop(0)
+
+def is_increasing_too_much(arr):
+  if len(arr) < num_increases_tolerated:
+    return False
+  prev = arr[0]
+  for x in arr[1:]:
+    if x < prev:
+      return False
+    prev = x
+  print('is_increasing_too_much is true for')
+  print(arr)
+  return True
 
 for epoch in range(200):
     for i, data in enumerate(dataloader, 0):
@@ -236,7 +326,7 @@ for epoch in range(200):
         input.resize_(real_cpu.size()).copy_(real_cpu)
         label.resize_(batch_size).fill_(real_label)
         inputv = Variable(input)
-        labelv = Variable(label)
+        labelv_real = Variable(label)
         ############################
         # (1) Update D_i networks: maximize log(D_i(x)) + log(1 - D_i(G(z)))
         ###########################
@@ -248,53 +338,79 @@ for epoch in range(200):
         loss_C_real = criterion(classes_predicted_real, img_context)
         loss_C_real.backward(retain_graph=True)
 
-        loss_Ds = torch.zeros((batch_size, nd)).type(dtype)
+        #loss_C_real_clone = loss_C_real.clone()
+    
+        # train with fake
+        fake_context_vector = generate_fake_context_tensor(batch_size)
+
+        labelv_fake = Variable(label.fill_(fake_label))
+        
+        noise.resize_(batch_size, noise.size(1), noise.size(2), noise.size(3)).normal_(0, 1)
+        noisev = Variable(noise)
+        fake = G(noisev, fake_context_vector) # fake context vecot should be passed here
+
+        #classes_predicted_fake = C(fake)
+        #loss_C_fake = criterion(classes_predicted_fake, fake_context_vector)
+        #loss_C_fake.backward(retain_graph=True)
+
+        #loss_C_fake_clone = loss_C_fake.clone()
+
+        #classes_predicted = C(fake)
+
+        loss_Ds_real = torch.zeros((batch_size, nd)).type(dtype)
+        loss_Ds_fake = torch.zeros((batch_size, nd)).type(dtype)
+
         for j, SNDx in enumerate(SND_list):
-            loss_Ds[:,j] = criterion(SNDx(inputv), labelv)
+          if losses_list[j] == 'BCE':
+            loss_Ds_real[:,j] = criterion(SNDx(inputv), labelv_real)
+            loss_Ds_fake[:,j] = criterion(SNDx(fake.detach()), labelv_fake)
+            #print('printing losses Ds real and fake for BCE')
+            #print(loss_Ds_real[:,j])
+            #print(loss_Ds_fake[:,j])
+          if losses_list[j] == 'W':
+            loss_Ds_real[:,j] = - SNDx(inputv) #w_loss_func_D(SNDx(inputv), SNDx(fake))
+            loss_Ds_fake[:,j] = SNDx(fake) #w_loss_func_D(SNDx(inputv), SNDx(fake))
+            #print('printing losses Ds real and fake for W')
+            #print(loss_Ds_real[:,j])
+            #print(loss_Ds_fake[:,j])
 
-        # TODO: add context - see conditional GANs (w/ classifier)
-        W = E(inputv, nd, img_context) # batchsize x nd
+        # loss_Ds_combined = 
 
-        kl_div = - alpha * torch.mean(torch.log(W))
-        loss_E = nd * (torch.mean(torch.mul(W, loss_Ds.detach() ) ) + kl_div)
-        loss_E.backward()
+        W_real = E(inputv, nd, img_context) # batchsize x nd
+
+        # just for printing and logging
+        Wmeans_real = torch.mean(W_real, dim=0)
+        bestD_real = torch.argmax(Wmeans_real)
+
+        kl_div = - alpha * torch.mean(torch.log(W_real))
+        loss_E = nd * (torch.mean(torch.mul(W_real, loss_Ds_real.detach() ) ) + kl_div)
+        loss_E.backward(retain_graph=True)
         optimizerE.step()
 
-        loss_D = nd * (torch.mean(loss_Ds))
+        #loss_D = nd * ( torch.mean(loss_Ds_real) + torch.mean(loss_Ds_fake) )
+        #loss_D.backward(retain_graph=True)
+        loss_D = nd * torch.mean(loss_Ds_real)
         loss_D.backward(retain_graph=True)
+        #loss_D = nd * torch.mean(loss_Ds_fake)
+        #loss_D.backward(retain_graph=True)
 
         E_G_z1 = loss_E.clone()
         D_G_z1 = loss_D.clone()
 
         for optimizerSNDx in optimizerSND_list:
             optimizerSNDx.step()
-
-        fake_context_vector = generate_fake_context_tensor(batch_size)
-
-        # train with fake
-        noise.resize_(batch_size, noise.size(1), noise.size(2), noise.size(3)).normal_(0, 1)
-        noisev = Variable(noise)
-        fake = G(noisev, fake_context_vector) # fake context vecot should be passed here
-        labelv = Variable(label.fill_(fake_label))
         
-        classes_predicted_fake = C(fake)
-        loss_C_fake = criterion(classes_predicted_fake, fake_context_vector)
-        loss_C_fake.backward(retain_graph=True)
+        W_fake = E(fake, nd, fake_context_vector) # batchsize x nd
 
-        loss_Ds = torch.zeros((batch_size, nd)).type(dtype)
-        for j, SNDx in enumerate(SND_list):
-            loss_Ds[:,j] = criterion(SNDx(fake.detach()), labelv)
-            #fakeD = SNDx(fake.detach())
-            #realD = SNDx(inputv)
-            #loss_Ds[:,j] = w_loss_func_D(realD, fakeD)
-
-        #fake_context_vector = [generate_fake_context_vector() for x in range(batch_size)]
-        fake_context_vector = generate_fake_context_tensor(batch_size)
-
-        W = E(fake, nd, fake_context_vector) # batchsize x nd
-
-        loss_D = nd * (torch.mean(loss_Ds))
+        loss_D = nd * (torch.mean(loss_Ds_fake))
         loss_D.backward(retain_graph=True)
+
+        # missing new loss_E
+
+        #kl_div = - alpha * torch.mean(torch.log(W_fake))
+        #loss_E = nd * (torch.mean(torch.mul(W_fake, loss_Ds_fake.detach() ) ) + kl_div)
+        #loss_E.backward(retain_graph=True)
+        #optimizerE.step()
 
         E_G_z2 = loss_E.clone()
         D_G_z2 = loss_D.clone()
@@ -325,30 +441,74 @@ for epoch in range(200):
 
             loss_Ds = torch.zeros((batch_size, nd)).type(dtype)
             for j, SNDx in enumerate(SND_list):
+              if losses_list[j] == 'BCE':
                 loss_Ds[:,j] = criterion(SNDx(fake), labelv)
+              if losses_list[j] == 'W': 
+                loss_Ds[:,j] = w_loss_func_G(SNDx(fake))
+                #print('loss_Ds computed by w_loss_func_G')
+                #print(loss_Ds[:,j])
 
-            #W = E(fake, nd, fake_context_vector) # batchsize x nd
-            #loss_G = nd * torch.mean(torch.mul(W, loss_Ds)) 
-            Wmeans = torch.mean(W, dim=0)
-            bestD = torch.argmax(Wmeans)
-            print('bestD is: ' + str(bestD))
+            #= torch.mult(W_fake, loss_Ds) 
+            Wmeans_fake = torch.mean(W_fake, dim=0)
+            bestD_fake = torch.argmax(Wmeans_fake)
+            print('bestD_fake is: ' + str(bestD_fake))
             #bestD = 0
-            loss_G = torch.mean(loss_Ds[bestD])
+            loss_G = torch.mean(loss_Ds[bestD_fake])
             #loss_G = w_loss_func_G()
             loss_G.backward(retain_graph=True)
 
             optimizerG.step()
+            if step % 20 == 0:
+                # print(W)
+                message = '[' + str(epoch) + '/' + str(200) + '][' + str(i) + '/' + str(len(dataloader)) + ']'
+                message += ' Loss_D: ' + ('{:.4f}'.format(torch.mean(loss_D)))
+                message += ' Loss_G: ' + ('{:.4f}'.format(loss_G.data.cpu().numpy())) 
+                message += ' E(G(z)): ' + ('{:.4f}'.format(E_G_z1.data.cpu().numpy())) + ' / ' + ('{:.4f}'.format(E_G_z2.data.cpu().numpy()))
+                message += ' D(G(z)): ' + ('{:.4f}'.format(D_G_z1.data.cpu().numpy())) + ' / ' + ('{:.4f}'.format(D_G_z2.data.cpu().numpy()))
+                #message += ' KL: ' + ('{:.4f}'.format(kl_div))
+                message += ' ' + logdir
+                print(message)
+ 
+                if opt.earlyterminate:
+                    loss_D_numeric = float('{:.4f}'.format(torch.mean(loss_D)))
+                    loss_G_numeric = float('{:.4f}'.format(loss_G.data.cpu().numpy()))
+                    if abs(loss_D_numeric) > 10:
+                        print('loss_D became too high')
+                        sys.exit()
+                    if abs(loss_G_numeric) > 50:
+                        print('loss_G became too high')
+                        sys.exit()
+                    append_and_ensure_length(loss_D_history, abs(loss_D_numeric))
+                    append_and_ensure_length(loss_G_history, abs(loss_G_numeric))
+                    if is_increasing_too_much(loss_D_history):
+                        print('loss_D increasing too much')
+                        sys.exit()
+                    if is_increasing_too_much(loss_G_history):
+                        print('loss_G increasing too row')
+                        sys.exit()
 
-        if i % 20 == 0:
-            # print(W)
-            message = '[' + str(epoch) + '/' + str(200) + '][' + str(i) + '/' + str(len(dataloader)) + ']'
-            message += ' Loss_D: ' + ('{:.4f}'.format(torch.mean(loss_D)))
-            message += ' Loss_G: ' + ('{:.4f}'.format(loss_G.data.cpu().numpy())) 
-            message += ' E(G(z)): ' + ('{:.4f}'.format(E_G_z1.data.cpu().numpy())) + ' / ' + ('{:.4f}'.format(E_G_z2.data.cpu().numpy()))
-            message += ' D(G(z)): ' + ('{:.4f}'.format(D_G_z1.data.cpu().numpy())) + ' / ' + ('{:.4f}'.format(D_G_z2.data.cpu().numpy()))
-            #message += ' KL: ' + ('{:.4f}'.format(kl_div))
-            message += ' ' + logdir
-            print(message)
+                if step % 200 == 0:
+                    data_to_write = {
+                    'epoch': epoch,
+                    #'loss_D': str(torch.mean(loss_D)),
+                    #'loss_C_real': str(loss_C_real_clone),
+                    #'loss_C_fake': str(loss_C_fake_clone),
+                    'loss_E_real': str(E_G_z1.data.cpu().numpy()),
+                    'loss_E_fake': str(E_G_z2.data.cpu().numpy()),
+                    'loss_D_real': str(D_G_z1.data.cpu().numpy()),
+                    'loss_D_fake': str(D_G_z2.data.cpu().numpy()),
+                    'bestD_fake': str(bestD_fake.data.cpu().numpy()),
+                    'bestD_real': str(bestD_real.data.cpu().numpy()),
+                    # output W as well
+                    }
+                    if epoch == 199:
+                        #data_to_write['bestD_fake'] = str(bestD_fake.data.cpu().numpy())
+                        data_to_write['fake_context_vector'] = fake_context_vector.detach().cpu().numpy().tolist()
+                        #data_to_write['bestD_real'] = str(bestD_real.data.cpu().numpy())
+                        data_to_write['real_context_vector'] = img_context.detach().cpu().numpy().tolist()
+                        loss_outfile.write(json.dumps(data_to_write))
+                        loss_outfile.write('\n')
+                        loss_outfile.flush()
 
             #print('[%d/%d][%d/%d] Loss_D1: %.4f Loss_D2: %.4f Loss_D3: %.4f Loss_G: %.4f = Loss_log(D(G(z))*E(X,c)) E(G(z)): %.4f / %.4f' % (epoch, 200, i, len(dataloader),
             #         errD1.data[0], errD2.data[0], errD3.data[0], errG.data[0], E_G_z1, E_G_z2))
@@ -369,3 +529,11 @@ for epoch in range(200):
         SND_x = SND_list[ix]
         torch.save(SND_x.state_dict(), logdir + '/celeba_netD_batch' + ip + '.pth')
     torch.save(E.state_dict(), logdir + '/celeba_netE_batch.pth')
+
+
+loss_outfile.close()
+finished_file = open(path.join(logdir, 'finished.txt'), 'wt')
+finished_file.write('finished')
+finished_file.close()
+
+print('finished')
